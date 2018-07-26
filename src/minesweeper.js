6c654f1ac96e387dea7765841c2df282e5e023b6
Marilyn DeSesa <marilyn.desesa@gmail.com>
2018-07-26 11:13:03 -0700

    added dynamic bomb board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,27 +1,3 @@
-/*const printBoard = board => {
-  console.log(`Current Board:`);
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
-};
-
-//create structure of game board
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-];
-
-//displays current board in console
-printBoard(board);
-
-//reassign value of nested array
-board[0][1] = '1';
-board[2][2] = 'B';
-
-//displays updated board in console
-printBoard(board);*/
-
 //create the player board
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   let board = [];
@@ -36,3 +12,30 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 };
 
 console.log(generatePlayerBoard(6,2));
+
+//create bomb board
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  return board;
+
+  //randomly generate bombs
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor (Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor (Math.random() * Columns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    //potential to add bombs over existing bombs - will address later
+  }
+};
+
+//logs the board to the consle
+const printBoard = (board) => {}
+console.log(generateBombBoard(3,2,2));
