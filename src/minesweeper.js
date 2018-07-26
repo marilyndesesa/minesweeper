63e31bcb3f52f5e27b7547936d2a6477d0eb30c4
Marilyn DeSesa <marilyn.desesa@gmail.com>
2018-07-26 10:59:57 -0700

    made gameboard dynamic

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,4 @@
-//create function to print the board correctly
-const printBoard = board => {
+/*const printBoard = board => {
   console.log(`Current Board:`);
   console.log(board[0].join(' | '));
   console.log(board[1].join(' | '));
@@ -21,4 +20,19 @@ board[0][1] = '1';
 board[2][2] = 'B';
 
 //displays updated board in console
-printBoard(board);
+printBoard(board);*/
+
+//create the player board
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
+
+console.log(generatePlayerBoard(6,2));
